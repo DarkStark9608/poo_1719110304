cbd788ada2dc8e8bf6eb3d99a4c27289a9e32ce2
DarkStark9608 <1719110304@utectulancingo.edu.mx>
2020-05-21 03:21:50 +0000

    Semana 2

--- Semana_2/CBanco.py
+++ Semana_2/CBanco.py
@@ -1,2 +1,14 @@
 class CBanco:
-  
+
+  def __init__(self,nombreBanco, RFC, tipoCuenta):
+    
+    
+    def aperturarCuenta():
+     pass
+    def cerrarCuenta():
+     pass
+    def editarDatos():
+      pass
+    def consultarSaldo():
+      pass
+
